5ec97fe6ba9ce812bfc81ec943b5b27ff83ee446
Aasik <aasikmohamed94@gmail.com>
2018-12-16 00:38:27 +0530

    Add files via upload

---
+++ JavaMyFirstApplication/src/firstApplication/FirstApplication.java
@@ -0,0 +1,9 @@
+package firstApplication;
+
+public class FirstApplication {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi I am new to Java ");
+		System.out.println("Software Development is my Passion");
+	}
+}
